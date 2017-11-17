7bb3beeadb612e861014b3bf27369a96fe02f9a9
gitalvininfo <yansonalvin29@gmail.com>
2017-11-17 08:34:39 +0800

    added sign up

--- webprog/index.php
+++ webprog/index.php
@@ -1,26 +1,20 @@
 <!DOCTYPE html>
 <html lang="en" class="body-full-height">
     <head>        
-        <!-- META SECTION -->
-        <title>Joli Admin - Responsive Bootstrap Admin Template</title>            
+        <title>Web Programming</title>            
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-
         <link rel="icon" href="favicon.ico" type="image/x-icon" />
-        <!-- END META SECTION -->
-
-        <!-- CSS INCLUDE -->        
         <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
-        <!-- EOF CSS INCLUDE -->                                     
     </head>
     <body>
 
-        <div class="login-container lightmode">
+        <div class="login-container">
 
             <div class="login-box animated fadeInDown">
                 <div class="login-body">
-                    <div class="login-title"><strong>Log In</strong> to your account</div>
+                    <div class="login-title"><strong>Welcome, </strong>login to your account</div>
                     <form action="index.html" class="form-horizontal" method="post">
                         <div class="form-group">
                             <div class="col-md-12">
@@ -34,11 +28,11 @@
                         </div>
                         <div class="form-group">
                             <div class="col-md-6">
-                                <a href="#" class="btn btn-link btn-block">Forgot your password?</a>
+                                <a href="home.php" class="btn btn-link btn-block">Forgot your password?</a>
                             </div>
                             <div class="col-md-6">
                                 <button type="submit" name="login" class="btn btn-info btn-block">Log In</button>
-                                
+
                             </div>
                         </div>
                         <div class="login-or">OR</div>
@@ -52,9 +46,9 @@
                             <div class="col-md-4">                            
                                 <button class="btn btn-info btn-block btn-google"><span class="fa fa-google-plus"></span> Google</button>
                             </div>
-                        </div>
+                        </div>s
                         <div class="login-subtitle">
-                            Don't have an account yet? <a href="#">Create an account</a>
+                            Don't have an account yet? <a href="sign_up.php">Create an account</a>
                         </div>
                     </form>
                 </div>
---
+++ webprog/sign_up.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+<html lang="en" class="body-full-height">
+    <head>        
+        <title>Web Programming</title>            
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="favicon.ico" type="image/x-icon" />
+        <link rel="stylesheet" type="text/css" id="theme" href="css/theme-default.css"/>
+    </head>
+    <body>
+
+        <div class="login-container">
+
+            <div class="login-box animated fadeInDown">
+                <div class="login-body">
+                    <div class="login-title"><strong>Sign Up</strong></div>
+                    <form action="index.html" class="form-horizontal" method="post">
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" placeholder="Name"/>
+                            </div>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" placeholder="Password"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <input type="text" class="form-control" placeholder="Username"/>
+                            </div>
+                            <div class="col-md-6">
+                                <input type="password" class="form-control" placeholder="Confirm Password"/>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-4">                                        
+                                <select class="form-control select" data-style="btn-primary">
+                                    <option>Day</option>
+                                    <option value="1">1</option>
+                                    <option value="2">2</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">                                        
+                                <select class="form-control select" data-style="btn-primary">
+                                    <option>Month</option>
+                                    <option value="Jan">Jan</option>
+                                    <option value="Feb">Feb</option>
+                                </select>
+                            </div>
+                            <div class="col-md-4">                                        
+                                <select class="form-control select" data-style="btn-primary">
+                                    <option>Year</option>
+                                    <option value="2000">2000</option>
+                                    <option value="2001">2001</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-12">                                        
+                                <select class="form-control select" data-style="btn-primary">
+                                    <option>Country</option>
+                                    <option value="Philippines">Philippines</option>
+                                    <option value="China">China</option>
+                                </select>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6">
+                                <a href="index.php" class="btn btn-link btn-block">Already have an account</a>
+                            </div>
+                            <div class="col-md-6">
+                                <button type="submit" name="login" class="btn btn-info btn-block">Create Account</button>
+
+                            </div>
+                        </div>
+
+                        <div class="login-subtitle">
+                            <div class="form-group">
+                                <div class="col-md-12">                                    
+                                    <label class="check"><input type="checkbox" class="icheckbox"/> I agree to the terms of use and privacy policy.</label>
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                <div class="login-footer">
+                    <div class="pull-left">
+                        &copy; 2014 AppName
+                    </div>
+                    <div class="pull-right">
+                        <a href="#">About</a> |
+                        <a href="#">Privacy</a> |
+                        <a href="#">Contact Us</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <script type="text/javascript" src="js/plugins/jquery/jquery.min.js"></script>
+        <script type="text/javascript" src="js/plugins/jquery/jquery-ui.min.js"></script>
+        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap.min.js"></script>                
+        <script type='text/javascript' src='js/plugins/icheck/icheck.min.js'></script>
+        <script type="text/javascript" src="js/plugins/bootstrap/bootstrap-select.js"></script>
+        <script type="text/javascript" src="js/plugins.js"></script>        
+        <script type="text/javascript" src="js/actions.js"></script>      
+    </body>
+</html>
+
+
+
+
+
+
